a199baaed030363de44e52d3f00f17df7c96a4f9
nibbu2822 <m.wesseler1998@gmail.com>
2026-05-03 23:56:46 +0200

    add gradle project clean

---
+++ blatt01/Aufgabe-Gradle/gradle_projekt/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!\n");
+        System.out.println(System.getProperty("java.version"));
+    }
+}
